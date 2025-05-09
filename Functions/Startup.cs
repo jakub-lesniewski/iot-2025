8dec2c37ca692175509cbdb1d2235b0cdb7a5bf9
jakubjanlesniewski@gmail.com <jakubjanlesniewski@gmail.com>
2025-05-09 22:10:48 +0200

    inital functions setup

---
+++ Functions/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using IndustrialIoT.Functions.Services;
+
+[assembly: FunctionsStartup(typeof(IndustrialIoT.Functions.Startup))]
+namespace IndustrialIoT.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddSingleton<IAzureIoTService, AzureIoTService>();
+        }
+    }
+}
